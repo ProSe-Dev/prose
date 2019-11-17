9475e536bfd11535a2489ded58c0e948737047e1
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-11-16 18:26:10 -0800

    user can now select and add new project folder

--- client/electron/project.js
+++ client/electron/project.js
@@ -1,43 +1,105 @@
 const { dialog, ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const {
+  SYNC_CREATE_PROJECT_CH,
+  PROJECT_CONFIG_FOLDER_NAME
+} = require('../src/shared/constants');
 
-const SYNC_CREATE_PROJECT_CH = 'sync-create-project';
-const ASYNC_CREATE_PROJECT_CH = 'async-create-project';
+exports.bootstrap = function () {
+  /**
+   * open dialog that lets user selects a directory
+   * @return String[] folder path selected by user | null if none is selected
+   */
+  function selectDirSync() {
+    let selectedPaths = dialog.showOpenDialogSync(null, {
+      title: 'Select Project Folder',
+      defaultPath: './',
+      buttonLabel: 'Select',
+      properties: ['openDirectory', 'multiSelections']
+    });
+    return selectedPaths ? selectedPaths[0] : null;
+  }
 
+  /**
+   * checks if a directory is already a prose project
+   * @param projectPath - absolute path to the project folder
+   * @return boolean
+   */
+  function isProseProject(projectPath) {
+    let configFolder = path.resolve(projectPath, PROJECT_CONFIG_FOLDER_NAME);
+    return fs.existsSync(configFolder);
+  }
 
-function selectDir() {
-  dialog.showOpenDialog(mainWindow, {
-    properties: ['openDirectory']
-  })
+  /** 
+   * create a new prose project in the give path 
+   * @param {String} projectPath - path to project
+   */
+  function createProseProject(projectPath) {
+    // create hidden directory to contain config files
+    let configFolder = path.resolve(projectPath, PROJECT_CONFIG_FOLDER_NAME);
+    if (!fs.existsSync(configFolder)) {
+      fs.mkdirSync(configFolder);
+    }
+
+    // TODO: [a04a] should create config files in .prose
+    // 1) project.json
+    // 2) certificate.json
+    // can probably do this async
+
+    // TODO: [3934] should grab files specified by the directory
+    // maybe use fs.Dir?
+    // https://nodejs.org/api/fs.html#fs_class_fs_dir
+    let files = [];
+
+    let project = {
+      name: path.basename(projectPath),
+      certificates: [],
+      path: projectPath,
+      files
+    }
+    return project;
+  }
+
+  /**
+   * given a existing prose project return its info
+   * @param {String} projectPath - path to project
+   */
+  function openProseProject(projectPath) {
+    // TODO: [1d64] should read config files in .prose
+    // 1) project.json
+    // 2) certificate.json
+
+    // TODO: [3934] should grab files specified by the directory
+    // maybe use fs.Dir?
+    // https://nodejs.org/api/fs.html#fs_class_fs_dir
+    let files = [];
+
+    let project = {
+      name: path.basename(projectPath),
+      certificates: [],
+      path: projectPath,
+      files
+    };
+
+    return project;
+  }
+
+  ipcMain.on(SYNC_CREATE_PROJECT_CH, (event, arg) => {
+    console.log(event, arg);
+
+    // display pop up for user to select the project folder
+    let projectPath = selectDirSync();
+
+    if (!projectPath) {
+      console.log('user folder selection');
+      event.returnValue = null;
+    } else if (isProseProject(projectPath)) {
+      let project = openProseProject(projectPath);
+      event.returnValue = project;
+    } else {
+      let project = createProseProject(projectPath);
+      event.returnValue = project;
+    }
+  });
 }
-
-// TODO: should creating project be sync. or async. ?
-// sync: easier to handle on the GUI
-// async: better UX / performance cuz GUI can be preloaded during wait 
-ipcMain.on(ASYNC_CREATE_PROJECT_CH, (event, arg) => {
-  console.log(event, arg);
-  // TODO: do create project work here
-  let projectInfo = {
-    name: 'VPN',
-    files: {},
-    certificate: null
-  };
-  setTimeout(5000, () => {
-    // send a async reply back to RP
-    event.reply(SYNC_CREATE_PROJECT_CH, projectInfo);
-  });
-});
-
-ipcMain.on(SYNC_CREATE_PROJECT_CH, (event, arg) => {
-  console.log(event, arg);
-  // TODO: do create project work here
-  let projectInfo = {
-    name: 'VPN',
-    files: {},
-    certificate: null
-  };
-
-  setTimeout(5000, () => {
-    // store result so RP can proceed
-    event.returnValue = projectInfo;
-  });
-});
--- client/src/containers/MainContent/pages/HomePage/HomePage.jsx
+++ client/src/containers/MainContent/pages/HomePage/HomePage.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { SYNC_CREATE_PROJECT_CH } from 'shared/constants.js';
+
+// workaround for served react app to get access to electron module
+// reference: https://github.com/electron/electron/issues/7300
+const ipc = window.require('electron').ipcRenderer;
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" class="btn btn-success">
+        <button 
+          type="button"
+          className="btn btn-success"
+          onClick={() => {
+            console.log('button pressed');
+            let result = ipc.sendSync(SYNC_CREATE_PROJECT_CH, 'testproject');
+            console.log(result);
+          }}
+        >
           ADD NEW PROJECT
         </button>
       </div>
     );
   }
-}
+};
 
 export default HomePage;
