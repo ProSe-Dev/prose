dbd4aec4c96a7baf727a5576cb02b4ed05a1b3eb
Paul Wang <paul_wang347@hotmail.com>
2019-11-29 15:42:08 -0800

    Rename

--- client/electron/ipcHandlers/project.js
+++ client/electron/ipcHandlers/project.js
@@ -1,135 +1,105 @@
 const { ipcMain } = require("electron");
-const { resolve, basename } = require("path");
-const settings = require('../settings');
-const fs = require("../helpers/fs");
-const filesHelper = require("../helpers/files");
-const { selectDirAsync, selectFileAsync } = require("../helpers/dialog");
-const Log = require("../helpers/log");
-const events = require("../../src/shared/ipc-events");
-const { generateUUID } = require('../helpers/id');
+const { selectDirAsync, selectFileAsync } = require("./helpers/dialog");
+const Log = require("./helpers/log");
+const { generateUUID } = require("./helpers/id");
 
-const PROJECT_CONFIG_FOLDER_NAME = ".prose";
-const PROJECT_SETTINGS_NAMESPACE = 'projects';
-const __projects = settings.get(PROJECT_SETTINGS_NAMESPACE);
-
-/**
-  * checks if a directory is already a prose project
-  * @param projectPath - absolute path to the project folder
-  * @return boolean
-  */
-function isProseProject(projectPath) {
-  let configFolder = resolve(projectPath, PROJECT_CONFIG_FOLDER_NAME);
-  return fs.existsAsync(configFolder);
-}
-
-/**
- * get the existing prose projects 
- */
-ipcMain.handle(events.GET_EXISTING_PROJECTS, (event, args) => {
-  return Object.keys(__projects).map(k => __projects[k]);
+const events = require("../src/shared/ipc-events");
+const color = require("../src/shared/color");
+const s = require("../src/shared/settings");
+const { Project } = require("./models/Project");
+const settings = require("electron-settings");
+const projectList = settings.get(s.PROJECTS_LIST, []);
+const projectIDMap = {};
+const projectPathMap = {};
+projectList.forEach(p => {
+  projectIDMap[p.projectID] = p;
+  projectPathMap[p.path] = p;
 });
 
-/**
- * get the files and certificate info of the project
- * args[0] - id of the project
- */
-ipcMain.handle(events.GET_PROJECT_INFO, async (event, args) => {
-  Log.ipcLog(events.GET_PROJECT_INFO, args);
-  let id = args[0];
-  let path = __projects[id].path;
-  let projectInfo = {};
-
-  if (!path) {
-    return null;
+exports.bootstrap = function() {
+  /**
+   * checks if a directory is already a prose project
+   * @param projectPath - absolute path to the project folder
+   * @return boolean
+   */
+  function isProseProject(projectPath) {
+    return projectList.some(p => {
+      return p.path === projectPath;
+    });
   }
 
-  projectInfo.files = await filesHelper.getAllFilesInDirAsync(path);
-  let snapshotsRaw = await fs.readFileAsync(
-    resolve(path, ".prose", "snapshots.json")
-  );
-  projectInfo.snapshots = JSON.parse(snapshotsRaw);
+  ipcMain.handle(events.GET_EXISTING_PROJECTS, (event, ...args) => {
+    Log.ipcLog(events.GET_EXISTING_PROJECTS, args);
+    Log.debugLog(JSON.stringify(projectList));
+    return projectList;
+  });
 
-  Log.debugLog(events.GET_PROJECT_INFO, projectInfo.files);
-  Log.debugLog(events.GET_PROJECT_INFO, projectInfo.snapshots);
+  ipcMain.handle(events.GET_PROJECT_INFO, async (event, ...args) => {
+    Log.ipcLog(events.GET_PROJECT_INFO, args);
+    let id = args[0];
+    Log.debugLog("Result: " + JSON.stringify(projectIDMap[id]));
+    return projectIDMap[id];
+  });
 
-  return projectInfo;
-});
+  ipcMain.handle(events.SELECT_FOLDER, async (event, ...args) => {
+    Log.ipcLog(events.SELECT_FOLDER, args);
+    let result = await selectDirAsync();
+    if (result.canceled) {
+      Log.ipcLog(events.SELECT_FOLDER, "canceled");
+      return null;
+    } else {
+      Log.ipcLog(events.SELECT_FOLDER, result);
+      return result.filePaths[0];
+    }
+  });
 
-/**
- * prompts user to select a folder with open dialog
- * @returns {Promise<String>} promise resolves folder path
- */
-ipcMain.handle(events.SELECT_FOLDER, async (event, ...args) => {
-  Log.ipcLog(events.SELECT_FOLDER, args);
-  let result = await selectDirAsync();
-  if (result.canceled) {
-    Log.ipcLog(events.SELECT_FOLDER, "canceled");
-    return null;
-  } else {
-    Log.ipcLog(events.SELECT_FOLDER, result);
-    return result.filePaths[0];
-  }
-});
+  ipcMain.handle(events.SELECT_FILE, async (event, ...args) => {
+    Log.ipcLog(events.SELECT_FILE, args);
+    let result = await selectFileAsync();
+    if (result.canceled) {
+      Log.ipcLog(events.SELECT_FILE, "canceled");
+      return null;
+    } else {
+      Log.ipcLog(events.SELECT_FILE, result);
+      return result.filePaths[0];
+    }
+  });
 
-/**
- * prompts user to select a file with open dialog
- * @returns {Promise<String>} promise resolves file path
- */
-ipcMain.handle(events.SELECT_FILE, async (event, ...args) => {
-  Log.ipcLog(events.SELECT_FILE, args);
-  let result = await selectFileAsync();
-  if (result.canceled) {
-    Log.ipcLog(events.SELECT_FILE, "canceled");
-    return null;
-  } else {
-    Log.ipcLog(events.SELECT_FILE, result);
-    return result.filePaths[0];
-  }
-});
+  ipcMain.handle(events.ADD_PROJECT, async (event, ...args) => {
+    Log.ipcLog(events.ADD_PROJECT, args);
+    let name = args[0];
+    let path = args[1];
+    let contact = args[2];
+    let project = null;
 
-/**
- * convert a given directory into a ProSe project! yay how fun
- * args[0] - name of project
- * args[1] - author contact info
- * args[2] - absolute path to project
- */
-ipcMain.handle(events.ADD_PROJECT, async (event, ...args) => {
-  Log.ipcLog(events.ADD_PROJECT, args);
-  let name = args[0];
-  let contact = args[1];
-  let path = args[2];
-  let projectPath = resolve(__dirname, path);
+    try {
+      // return if folder is already a prose project
+      if (isProseProject(path)) {
+        return;
+      }
 
-  try {
-    // return if folder is already a prose project
-    // if (await isProseProject(projectPath)) {
-    //   console.log(events.ADD_PROJECT, "already a prose project");
-    //   return;
-    // }
+      project = new Project(
+        generateUUID(),
+        name,
+        contact,
+        path,
+        new Date(),
+        color.randomColorClass()
+      );
 
-    // set up .prose folder
-    let proseFolder = resolve(projectPath, PROJECT_CONFIG_FOLDER_NAME);
-    await fs.mkdirAsync(proseFolder);
+      projectList.push(project);
+      projectIDMap[project.projectID] = project;
+      projectPathMap[project.path] = project;
+      settings.set(s.PROJECTS_LIST, projectList);
+      Log.debugLog(JSON.stringify(projectIDMap));
 
-    // set up project config file
-    let project = {
-      projectId: generateUUID(),
-      projectName: name,
-      projectPath,
-      authorContact: contact,
-      createdOn: new Date().toISOString()
-    };
-    await fs.writeFileAsync(
-      resolve(proseFolder, "project.json"),
-      JSON.stringify(project)
-    );
+      // TODO: should set up git here
+    } catch (err) {
+      project = null;
+      Log.debugLog(err);
+    }
 
-    Log.debugLog(events.ADD_PROJECT, 'project: ' + JSON.stringify(project));
-
-    settings.add(PROJECT_SETTINGS_NAMESPACE, project.projectId, project);
-    // TODO: should set up git here
+    Log.debugLog(JSON.stringify(project));
     return project;
-  } catch (err) {
-    console.log(err);
-  }
-});
+  });
+};
