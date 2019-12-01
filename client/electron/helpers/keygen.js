3c945a5d386d0f2e59c2f7e5f2b99a42dcf84d22
Paul Wang <paul_wang347@hotmail.com>
2019-11-30 17:56:41 -0800

    Hook working now

--- client/electron/helpers/keygen.js
+++ client/electron/helpers/keygen.js
@@ -3,14 +3,13 @@ const crypto = require("crypto");
 function createKeyPair(secret) {
   let _privateKeyEncoding = {
     type: "pkcs8",
-    format: "pem",
+    format: "der"
   };
   if (secret != null) {
     _privateKeyEncoding.cipher = "aes-256-cbc";
     _privateKeyEncoding.passphrase = secret;
   }
   let result = crypto.generateKeyPairSync("ed25519", {
-    modulusLength: 4096,
     publicKeyEncoding: {
       type: "spki",
       format: "pem"
@@ -18,7 +17,7 @@ function createKeyPair(secret) {
     privateKeyEncoding: _privateKeyEncoding
   });
   return result;
-};
+}
 
 module.exports = {
   createKeyPair
