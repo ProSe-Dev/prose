a31d67aa1d91674cf11055d2d3c78d23d20a9a90
Paul Wang <paul_wang347@hotmail.com>
2019-12-04 20:11:28 -0800

    Windows demo fix

--- client/electron/helpers/environment.js
+++ client/electron/helpers/environment.js
@@ -1,22 +1,22 @@
-const { platform } = require('os');
+const { platform } = require("os");
 const appRoot = process.env.PWD;
-const inDevelopment = process.env.NODE_ENV === 'develpoment';
-const inDemo = process.env.NODE_ENV === 'demo';
+const inDevelopment = process.env.NODE_ENV === "development";
+const inDemo = process.env.NODE_ENV === "demo";
 const inProduction = !process.env.NODE_ENV;
 
 function getPlatform() {
   switch (platform()) {
-    case 'aix':
-    case 'freebsd':
-    case 'linux':
-    case 'openbsd':
-    case 'android':
-      return 'linux';
-    case 'darwin':
-    case 'sunos':
-      return 'mac';
-    case 'win32':
-      return 'win';
+    case "aix":
+    case "freebsd":
+    case "linux":
+    case "openbsd":
+    case "android":
+      return "linux";
+    case "darwin":
+    case "sunos":
+      return "mac";
+    case "win32":
+      return "win";
     default:
       return null;
   }
